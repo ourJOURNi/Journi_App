35de5679729db6364af1805a0efd3320283cadfb
Eddie <eddielacrosse2@gmail.com>
2023-01-25 22:30:53 -0500

    added stepper to  to loign

--- lib/login/register-page.dart
+++ lib/login/register-page.dart
@@ -20,112 +20,152 @@ class _RegisterFormState extends State<RegisterForm> {
   final lastNameCTRL = TextEditingController();
   final emailCTRL = TextEditingController();
   final passwordCTRL = TextEditingController();
+  int _index = 0;
 
   @override 
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: <Widget>[
-          verticalInputDivider,
-          Padding(
-            padding: inputPadding,
+    return  
+      Stepper(
+      currentStep: _index,
+      onStepCancel: () {
+        if (_index > 0) {
+          setState(() {
+            _index -= 1;
+          });
+        }
+      },
+      onStepContinue: () {
+        if (_index <= 0) {
+          setState(() {
+            _index += 1;
+          });
+        }
+      },
+      onStepTapped: (int index) {
+        setState(() {
+          _index = index;
+        });
+      },
+      steps: <Step>[
+        Step(
+          title: const Text('Step 1 title'),
+          content: SizedBox(
+            width: 300,
+            height: 380,
             child: 
-              TextFormField(
-                autofocus: true,
-                controller: firstNameCTRL,
-                onChanged: (text) {
-                  firstName = text;
-                  print('First Name: ${firstName}');
-                },
-                decoration: const InputDecoration(
-                  hintText: 'First Name',
+          Form(
+            key: _formKey,
+            child:
+            Column(
+              children: <Widget>[
+                verticalInputDivider,
+                Padding(
+                  padding: inputPadding,
+                  child: 
+                    TextFormField(
+                      autofocus: true,
+                      controller: firstNameCTRL,
+                      onChanged: (text) {
+                        firstName = text;
+                        print('First Name: ${firstName}');
+                      },
+                      decoration: const InputDecoration(
+                        hintText: 'First Name',
+                      ),
+                      validator: (String? value) {
+                        return (value != null && value.contains('@')) ? 'Do not use the @ char.' : null;
+                      },
+                    ),
                 ),
-                validator: (String? value) {
-                  return (value != null && value.contains('@')) ? 'Do not use the @ char.' : null;
-                },
-              ),
-          ),
-          Padding(
-            padding: inputPadding,
-            child: 
-              TextFormField(
-                controller: lastNameCTRL,
-                onChanged: (text) {
-                  lastName = text;
-                  print('Last Name: ${lastName}');
-                },
-                decoration: const InputDecoration(
-                  hintText: 'Last Name',
+                Padding(
+                  padding: inputPadding,
+                  child: 
+                    TextFormField(
+                      controller: lastNameCTRL,
+                      onChanged: (text) {
+                        lastName = text;
+                        print('Last Name: ${lastName}');
+                      },
+                      decoration: const InputDecoration(
+                        hintText: 'Last Name',
+                      ),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                    ),
                 ),
-                validator: (String? value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-              ),
-          ),
-          Padding(
-            padding: inputPadding,
-            child: 
-              TextFormField(
-                controller: emailCTRL,
-                onChanged: (text) {
-                  email = text;
-                  print('Email: ${email}');
-                },
-                decoration: const InputDecoration(
-                  hintText: 'Email',
+                Padding(
+                  padding: inputPadding,
+                  child: 
+                    TextFormField(
+                      controller: emailCTRL,
+                      onChanged: (text) {
+                        email = text;
+                        print('Email: ${email}');
+                      },
+                      decoration: const InputDecoration(
+                        hintText: 'Email',
+                      ),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                    ),
                 ),
-                validator: (String? value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-              ),
-          ),
-          Padding(
-            padding: inputPadding,
-            child: 
-              TextFormField(
-                controller: passwordCTRL,
-                onChanged: (text) {
-                  password = text;
-                  print('Password ${password}');
-                },
-                decoration: const InputDecoration(
-                  hintText: 'Password',
+                Padding(
+                  padding: inputPadding,
+                  child: 
+                    TextFormField(
+                      controller: passwordCTRL,
+                      onChanged: (text) {
+                        password = text;
+                        print('Password ${password}');
+                      },
+                      decoration: const InputDecoration(
+                        hintText: 'Password',
+                      ),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                    ),
                 ),
-                validator: (String? value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-              ),
-          ),
-          expandedBottomOfPageDivider,
-          Container(
-              margin: const EdgeInsets.only(top: 20, bottom: 50),
-              child: ElevatedButton(
-                onPressed: () {
-                  register(firstName, lastName, email, password, context, firstNameCTRL, lastNameCTRL, emailCTRL, passwordCTRL)
-                    .then((value) => {
-                      firstName = "",
-                      lastName = "",
-                      email = "",
-                      password = "",
-                    });
-                  
-                },
-                style: buttonGreenStyle,
-                child: const Text('Submit', style: TextStyle(fontSize: 16)),
-              ),
+                expandedBottomOfPageDivider,
+                Container(
+                    margin: const EdgeInsets.only(top: 20, bottom: 50),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        register(firstName, lastName, email, password, context, firstNameCTRL, lastNameCTRL, emailCTRL, passwordCTRL)
+                          .then((value) => {
+                            firstName = "",
+                            lastName = "",
+                            email = "",
+                            password = "",
+                          });
+
+                      },
+                      style: buttonGreenStyle,
+                      child: const Text('Submit', style: TextStyle(fontSize: 16)),
+                    ),
+                  ),
+              ],
             ),
-        ],
-      ),
+          )),
+        ),
+        const Step(
+          title: Text('Step 2 title'),
+          content: Text('Content for Step 2'),
+        ),
+      ],
     );
+    
+    ;
   }
 }
